bbe2b77c5ae8149532bd89130d59e6b105eb3b29
Pietro Monticone <pietromonticone.cs@gmail.com>
2020-07-26 16:45:01 +0200

    Julia Module
    The DigitalEpidemiology.jl module has been reshaped and fully documented.

--- Modelling/Julia/.ipynb_checkpoints/DigitalEpidemiology-checkpoint.jl
+++ Modelling/Julia/.ipynb_checkpoints/DigitalEpidemiology-checkpoint.jl
@@ -1,46 +1,66 @@
+# DIGITAL EPIDEMIOLOGY JULIA PACKAGE 
+# AUTHORS: Monticone Pietro, Orsenigo Davide 
+# LAST UPDATE: 26-07-2020
+
 module DigitalEpidemiology
 
-# Data Management 
-using DataFrames, DataFramesMeta
-using DrWatson
+# REQUIRED PACKAGES 
 
-# Statistics
-using Random
-using Distributions
-using StatsBase
+## INSTALL 
+###ENV["PYTHON"] = "path/to/python"
+###Pkg.build("PyCall")
 
-# Graphs 
-using LightGraphs, SimpleWeightedGraphs
-using GraphPlot
- #using Graphs
+## IMPORT 
+using DataFrames, DataFramesMeta, DrWatson         # Data Management 
+using Random, Distributions, StatsBase             # Statistics
+using LightGraphs, SimpleWeightedGraphs, GraphIO   # Graphs 
+using GraphPlot, Plots, AgentsPlots, PlotThemes    # Data Visualization
+using PyCall, PyPlot                               # Python
+nx = pyimport("networkx")                          # Python
+np = pyimport("numpy")                             # Python
+nw = pyimport("netwulf")                           # Python
 
-# Modelling
-using Agents
+# FUNCTIONS 
 
-# Data Visualization
-using Plots
-using AgentsPlots
-using PlotThemes
-
-# Python
-# ENV["PYTHON"] = "path/to/python"
-# Pkg.build("PyCall")
-using PyCall
-using PyPlot
-nx = pyimport("networkx")
-np = pyimport("numpy")
-hvnx = pyimport("hvplot.networkx")
-nw = pyimport("netwulf");
-
-
+### Convert weighted directed LightGraphs into weighted directed NetworkX 
 function LightGraphs_to_NetworkX(G)
-    H = nx.DiGraph()
-    edgelist = []
+    # instantiate directed graph object 
+    H = nx.DiGraph()  
+    # create empty edgelist 
+    edgelist = []     
+    # fill edgelist 
     for row in eachrow(DataFrame([edge for edge in edges(G)]))
         push!(edgelist, (row[1], row[2], row[3]))
     end
+    # build NetworkX weighted directed graph from edgelist 
     H.add_weighted_edges_from(edgelist)
     return H
 end
 
+### Convert directed LightGraphs into directed NetworkX 
+function LG_to_NX(G)
+    # instantiate directed graph object 
+    H = nx.DiGraph()
+    # create empty edgelist 
+    edgelist = []
+    # fill edgelist 
+    for row in eachrow(DataFrame([edge for edge in edges(G)]))
+        push!(edgelist, (row[1], row[2]))
+    end
+    # build NetworkX weighted directed graph from edgelist 
+    H.add_edges_from(edgelist)
+    return H
 end
+
+### Convert weighted directed NetworkX into weighted directed LightGraphs 
+function NetworkX_to_LightGraphs(H)
+    # conveniently relable nodes
+    nx.convert_node_labels_to_integers(H, first_label=1)
+    # write NX edgelist
+    nx.write_edgelist(H, "graph.edgelist")
+    # read LG edgelist
+    G = loadgraph("graph.edgelist", GraphIO.EdgeList.EdgeListFormat())
+    return SimpleWeightedDiGraph(G)
+end
+
+end # Moudule
